f58f46f37e809924ac36a098fe379ea55de1f8e9
Martin Wimpress <code@flexion.org>
2013-12-16 16:11:38 +0000

    Fixed build order.

--- builder.sh
+++ builder.sh
@@ -34,12 +34,12 @@ MATE_BUILD_ORDER=(
     mate-file-manager
     mate-polkit
     mate-window-manager
+    mate-settings-daemon-gstreamer
+    mate-settings-daemon-pulseaudio
     mate-session-manager
     mate-menus
     mate-panel
-    mate-settings-daemon-gstreamer
     mate-media-gstreamer
-    mate-settings-daemon-pulseaudio
     mate-media-pulseaudio
     mate-backgrounds
     mate-themes
