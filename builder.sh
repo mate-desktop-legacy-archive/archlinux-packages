11e9bbc86b311ce13ff3eac85d50ee9aaa39367d
Martin Wimpress <code@flexion.org>
2013-12-18 15:22:18 +0000

    Typo.

--- builder.sh
+++ builder.sh
@@ -153,7 +153,7 @@ function tree_build() {
     local PKGBUILD=${PKGBUILD_VER}-${PKGBUILD_REL}
     local EXISTS=$(ls -1 *${PKGBUILD}*.pkg.tar.xz 2>/dev/null)
 
-    if [ "${PKG}" == "mate-settings-daemon-pulseaudio" ] || [ "${PKG}" == "mate-media-pulseaudio" ];; then
+    if [ "${PKG}" == "mate-settings-daemon-pulseaudio" ] || [ "${PKG}" == "mate-media-pulseaudio" ]; then
         sudo pacman -Rsdd --noconfirm mate-settings-daemon-gstreamer
         sudo pacman -Rsdd --noconfirm mate-media-gstreamer
     elif [ "${PKG}" == "mate-settings-daemon-gstreamer" ] || [ "${PKG}" == "mate-media-gstreamer" ]; then
